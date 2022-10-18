9cced7fdc9c6eda0c98f837eabc901308c00df85
momooo7 <116044626+momooo7@users.noreply.github.com>
2022-10-18 08:46:11 +0800

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -18,7 +18,7 @@ Widgetsmith、vision、Percento、Airmail、Usage、Spark、Pdf Viewer、谜底�
 [rewrite_local]
 
 # ～ RevenueCat（2022-10-12）@ddgksf2013
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-echo-response https://github.com/ddgksf2013/Cuttlefish/raw/master/Crack/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-echo-response https://raw.githubusercontent.com/momooo7/Crack/main/revenuecat.js
 
 [mitm]
 
